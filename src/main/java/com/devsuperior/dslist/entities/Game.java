a80cc31fb1677c65e74b1125f555a5f8ba27052b
Mateus Vasconcellos <souzavasconcellos1994@gmail.com>
2023-05-08 23:32:31 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -18,93 +18,127 @@ public class Game {
 
     @Column(name = "game_year")
     private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-public Game(){
+    public Game() {
 
-}
+    }
 
-public Game(long id, String title, Integer year, String imgUrl, String shortDescription, String longDescription) {
-    this.id = id;
-    this.title = title;
-    this.year = year;
-    this.imgUrl = imgUrl;
-    this.shortDescription = shortDescription;
-    this.longDescription = longDescription;
-}
+    public Game(long id, String title, Integer year, String genre, String platform, double score, String imgUrl,
+            String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platform;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
 
-public long getId() {
-    return id;
-}
+    public long getId() {
+        return id;
+    }
 
-public void setId(long id) {
-    this.id = id;
-}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-public String getTitle() {
-    return title;
-}
+    public String getTitle() {
+        return title;
+    }
 
-public void setTitle(String title) {
-    this.title = title;
-}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-public Integer getYear() {
-    return year;
-}
+    public Integer getYear() {
+        return year;
+    }
 
-public void setYear(Integer year) {
-    this.year = year;
-}
+    public void setYear(Integer year) {
+        this.year = year;
+    }
 
-public String getImgUrl() {
-    return imgUrl;
-}
+    public String getGenre() {
+        return genre;
+    }
 
-public void setImgUrl(String imgUrl) {
-    this.imgUrl = imgUrl;
-}
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
-public String getShortDescription() {
-    return shortDescription;
-}
+    public String getPlatform() {
+        return platforms;
+    }
 
-public void setShortDescription(String shortDescription) {
-    this.shortDescription = shortDescription;
-}
+    public void setPlatform(String platform) {
+        this.platforms = platform;
+    }
 
-public String getLongDescription() {
-    return longDescription;
-}
+    public double getScore() {
+        return score;
+    }
 
-public void setLongDescription(String longDescription) {
-    this.longDescription = longDescription;
-}
+    public void setScore(double score) {
+        this.score = score;
 
-@Override
-public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + (int) (id ^ (id >>> 32));
-    return result;
-}
+    }
 
-@Override
-public boolean equals(Object obj) {
-    if (this == obj)
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Game other = (Game) obj;
+        if (id != other.id)
+            return false;
         return true;
-    if (obj == null)
-        return false;
-    if (getClass() != obj.getClass())
-        return false;
-    Game other = (Game) obj;
-    if (id != other.id)
-        return false;
-    return true;
-}
-
-
+    }
 
 }
